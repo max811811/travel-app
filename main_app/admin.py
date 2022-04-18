252a000fd865b35feb2e6922d65c17c43b7121fd
mpomento <mpomento@gmail.com>
2022-04-18 15:53:23 -0700

    adding the attraction details page

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Destination
+from .models import Destination, Attraction, Review
 
 admin.site.register(Destination)
+admin.site.register(Attraction)
+admin.site.register(Review)
