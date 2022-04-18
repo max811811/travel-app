f189fa2d3f7ee2d07d61b48b8afdc49095cd2603
Sam-Paschal-731 <sapaschal731@gmail.com>
2022-04-18 09:43:29 -0700

    Destination model

--- main_app/models.py
+++ main_app/models.py
@@ -4,8 +4,13 @@ from django.urls import reverse
 
 # Create your models here.
 class Destination(models.Model):
+    city = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)
+    time_zone = models.CharField(max_length=20)
+    location_description = models.TextField(max_length=200)
   # Add the foreign key linking to a user instance
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
 
