5b4b8ed74514f77722cea5a47ae81c52ea7be0ae
Hannah Graves <hannahmgraves@gmail.com>
2022-04-18 09:42:30 -0700

    adding index and about pages. updating base.html

--- main_app/models.py
+++ main_app/models.py
@@ -3,9 +3,12 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 # Create your models here.
+
+
 class Destination(models.Model):
   # Add the foreign key linking to a user instance
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
 
