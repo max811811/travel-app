d97544b6d2edfc6e2442672608c5625dea116a16
max811811 <max.mcnally@outlook.com>
2022-04-21 15:07:43 -0700

    add security to api

--- main_app/services.py
+++ main_app/services.py
@@ -4,12 +4,12 @@ import requests
 
 def get_temptimezone(city):
     geocoding = requests.post(
-        f"https://maps.googleapis.com/maps/api/geocode/json?address={city}&key=AIzaSyC6L-lKIsvE0uRCMwLJKrITheGumyORIME")
+        f"https://maps.googleapis.com/maps/api/geocode/json?address={city}&key={os.getenv('DO_ACCESS_TOKEN')}")
     transgeocodes = geocoding.json()
     lat = transgeocodes["results"][0]["geometry"]["location"]["lat"]
     lon = transgeocodes["results"][0]["geometry"]["location"]["lng"]
     allWeatherDataJson = requests.post(
-        f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=b70e5243385456b84556f52c7008f578")
+        f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={os.getenv('DO_ACCESS_TOKEN2')}")
     allWeatherData = allWeatherDataJson.json()
     temperatureKelvins = allWeatherData["main"]["temp"]
     temperatureF = int((temperatureKelvins-273.15)*9/5+32)
@@ -20,12 +20,12 @@ def get_temptimezone(city):
 
 def weather_description(city):
     geocoding = requests.post(
-        f"https://maps.googleapis.com/maps/api/geocode/json?address={city}&key=AIzaSyC6L-lKIsvE0uRCMwLJKrITheGumyORIME")
+        f"https://maps.googleapis.com/maps/api/geocode/json?address={city}&key={os.getenv('DO_ACCESS_TOKEN')}")
     transgeocodes = geocoding.json()
     lat = transgeocodes["results"][0]["geometry"]["location"]["lat"]
     lon = transgeocodes["results"][0]["geometry"]["location"]["lng"]
     allWeatherDataJson = requests.post(
-        f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=b70e5243385456b84556f52c7008f578")
+        f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={os.getenv('DO_ACCESS_TOKEN2')}")
     allWeatherData = allWeatherDataJson.json()
     city_weather = allWeatherData["weather"][0]["description"]
     return city_weather
